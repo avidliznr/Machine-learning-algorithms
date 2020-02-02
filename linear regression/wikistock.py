6b74ae5ac64a545cf9c8ee7f986fa47a6fe840c0
avidliznr <37923934+avidliznr@users.noreply.github.com>
2020-02-02 20:34:32 +0530

    Update wikistock.py

--- linear regression/wikistock.py
+++ linear regression/wikistock.py
@@ -29,8 +29,6 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 #print(df.head())
 #print(df.tail())
 
-# x -> features y-> labels
-
 x = np.array(df.drop(['label'],1))
 x = preprocessing.scale(x)
 x_lately = x[-forecast_out:]
@@ -42,20 +40,9 @@ y = np.array(df['label'])
 #print(len(x),len(y))
 x_train,x_test,y_train,y_test = cross_validation.train_test_split(x,y,test_size=.2)
 
-#change the below line to change the type of model used
-#clf = LinearRegression()
-
 clf = LinearRegression(n_jobs=-1)
-#n_jobs defines how many jobs can be threaded at the same time..it differs form model to model..some may not even be treadable
-#-1 runs as many as possible
-#we are using the linear regression classifier 
-# this line of code uses the svm regresssion classifers
-#clf = svm.SVR() #default kernel is linear we can set it to different values
-#clf = svm.SVR(kernel = 'poly')
 
 clf.fit(x_train,y_train)
-#after training the dataset it is being saved so that we need not retrain the model every time.now after training it once we can use the
-#pickeled form to make the prediction rather than retraining the model
 with open('linearregression.pickle','wb') as f:
 	pickle.dump(clf,f)
 
@@ -63,13 +50,6 @@ pickle_in = open('linearregression.pickle','rb')
 clf = pickle.load(pickle_in)
 
 accuracy = clf.score(x_test,y_test)
-#accuracy in 0.97... for linear regression
-#for svm.SVR it is 0.807...
-#for polynomial kernel it is 0.062..so  worse..
-
-#print(accuracy) # shifted one percent of the data we get an accuracy of this value|| one percent came from the .01*.... .01*100 = 1
-#accuracy is actually error squared
-
 forecast_set = clf.predict(x_lately)
 
 print(forecast_set,accuracy,forecast_out)
